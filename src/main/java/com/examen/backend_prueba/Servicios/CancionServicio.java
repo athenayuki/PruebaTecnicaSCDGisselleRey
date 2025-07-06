0909be1a847221c793a49c131ed4d08da9c3f84b
athenayuki <gisse.rey@gmail.com>
2025-07-05 22:10:09 -0400

    capa cancion servicio

--- src/main/java/com/examen/backend_prueba/Servicios/CancionServicio.java
+++ src/main/java/com/examen/backend_prueba/Servicios/CancionServicio.java
@@ -1,8 +1,44 @@
 package com.examen.backend_prueba.Servicios;
 
 import org.springframework.stereotype.Service;
+import com.examen.backend_prueba.Repositorio.CancionRepositorio;
+import com.examen.backend_prueba.CancionEntidad;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CancionServicio {
+private final CancionRepositorio cancionRepositorio;
+
+    public CancionServicio(CancionRepositorio cancionRepositorio) {
+        this.cancionRepositorio = cancionRepositorio;
+    }
+
+    public CancionEntidad guardarCancion(CancionEntidad cancion) {
+        return cancionRepositorio.save(cancion);
+    }
+
+    public List<CancionEntidad> obtenerTodasLasCanciones() {
+        return cancionRepositorio.findAll();
+    }
+
+    public Optional<CancionEntidad> obtenerCancionPorId(int id) {
+        return cancionRepositorio.findById(id);
+    }
+
+    public void eliminarCancion(int id) {
+        cancionRepositorio.deleteById(id);
+    }
+
+    public CancionEntidad actualizarCancion(int id, CancionEntidad cancionActualizada) {
+        if (cancionRepositorio.existsById(id)) {
+            cancionActualizada.setIdCancion(id);
+            return cancionRepositorio.save(cancionActualizada);
+        } else {
+            throw new RuntimeException("Canción no encontrada con ID: " + id);
+        }
+    }
+
+
 
 }
