ad5d37d5fa03b3197367e0bd78f96339f7a1b0d0
athenayuki <gisse.rey@gmail.com>
2025-07-06 22:09:41 -0400

    nueva prueba sequence a identity

--- src/main/java/com/examen/backend_prueba/CancionEntidad.java
+++ src/main/java/com/examen/backend_prueba/CancionEntidad.java
@@ -10,40 +10,36 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 
-@Entity(name = "cancion") 
-@Table(name = "canciones") 
+@Entity(name = "cancion")
+@Table(name = "canciones")
 public class CancionEntidad {
 
-
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cancion_seq_gen") 
-    @SequenceGenerator(name = "cancion_seq_gen",sequenceName = "canciones_id_seq",
-                    initialValue = 1, allocationSize = 50 ) 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Cambiado de SEQUENCE a IDENTITY
     private int idCancion;
 
 
-    @Column(name = "TITULO_CANCION", length = 200, nullable = false) 
-    private String tituloCancion; 
+    @Column(name = "TITULO_CANCION", length = 200, nullable = false)
+    private String tituloCancion;
 
-    @Column(name = "CALIFICACION_CANCION", nullable = false) 
-    private double calificacionCancion; 
+    @Column(name = "CALIFICACION_CANCION", nullable = false)
+    private double calificacionCancion;
 
     @JsonFormat(pattern = "dd-MM-yyyy")
     @Column (name = "LANZAMIENTO_CANCION", nullable = false)
-    private LocalDate lanzamientoCancion; 
+    private LocalDate lanzamientoCancion;
 
     @Column(name = "FAVORITA_CANCION", nullable = false)
-    private boolean favoritaCancion; 
+    private boolean favoritaCancion;
 
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "idArtista")
     private ArtistaEntidad artista;
- 
+
 
      public CancionEntidad() {
     }
@@ -59,45 +55,44 @@ public class CancionEntidad {
 
     public int getIdCancion() {
         return idCancion;
-    }       
+    }
     public void setIdCancion(int idCancion) {
             this.idCancion = idCancion;
-    }   
+    }
 
     public String getTituloCancion() {
         return tituloCancion;
     }
-    public void setTituloCancion(String tituloCancion) {    
+    public void setTituloCancion(String tituloCancion) {
         this.tituloCancion = tituloCancion;
     }
 
     public double getCalificacionCancion() {
-        return calificacionCancion; 
+        return calificacionCancion;
     }
-    public void setCalificacionCancion(double calificacionCancion) {        
+    public void setCalificacionCancion(double calificacionCancion) {
         this.calificacionCancion = calificacionCancion;
     }
 
     public LocalDate getLanzamientoCancion() {
-        return lanzamientoCancion;      
+        return lanzamientoCancion;
     }
-    public void setLanzamientoCancion(LocalDate lanzamientoCancion) {                       
+    public void setLanzamientoCancion(LocalDate lanzamientoCancion) {
         this.lanzamientoCancion = lanzamientoCancion;
     }
 
     public boolean isFavoritaCancion() {
-        return favoritaCancion;             
+        return favoritaCancion;
     }
-    public void setFavoritaCancion(boolean favoritaCancion) {   
+    public void setFavoritaCancion(boolean favoritaCancion) {
         this.favoritaCancion = favoritaCancion;
     }
-    
-    public ArtistaEntidad getArtista() {
-        return artista; 
-    }
-    public void setArtista(ArtistaEntidad artista) {    
-        this.artista = artista;
-    }   
-}
 
+    public ArtistaEntidad getArtista() {
+        return artista;
+    }
+    public void setArtista(ArtistaEntidad artista) {
+        this.artista = artista;
+    }
+}
         
